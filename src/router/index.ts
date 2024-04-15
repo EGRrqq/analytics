9edec802aa06a653b151db8508ab58fcd450e199
EGRrqq <egrrqqdev@gmail.com>
2024-04-15 07:26:05 -0400

    feat: add analytics view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import SignUpView from "@/views/SignUpView.vue";
+import AnalyticsView from "@/views/AnalyticsView.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "signUp",
     component: SignUpView,
+  },
+  {
+    path: "/analytics",
+    name: "analytics",
+    component: AnalyticsView,
   },
 ];
 
---
+++ src/views/AnalyticsView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>sweet analytics</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default class AnalyticsView extends Vue {}
+</script>
